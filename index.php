2d8ac1b03f1b3d597c6b1dba7bf29ea811142245
Jessica Görög <Jessica.Goeroeg@gmail.com>
2021-04-27 22:08:51 +0200

    Added basic navbar

--- index.php
+++ index.php
@@ -15,6 +15,9 @@ require_once "utility/Validation.class.php";
 session_set_cookie_params(0, "/", $_SERVER['HTTP_HOST'], (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'on'), true);
 session_start();
 
+//BOOL FOR DEBUGGIG MODE
+$showDebug = false;
+
 
 // GET LOGIN STATUS
 $loggedIn = false;
@@ -42,28 +45,28 @@ if (isset($_COOKIE['sessionCookie'])) {
 }
 
 /// DEBUGGING
-// Debug login
-
-if (isset($_GET['debugLogin'])) {
-    $_SESSION['debugLogin'] = $_GET['debugLogin'];
-}
-if (isset($_SESSION['debugLogin'])) {
-    $loggedIn = $_SESSION['debugLogin'];
-    // Debug role (Only available if user is logged in)
-    if ($loggedIn) {
-        if (isset($_GET['debugRole'])) {
-            $_SESSION['debugRole'] = $_GET['debugRole'];
-        }
-        if (isset($_SESSION['debugRole'])) {
-            $userType = new UserType($_SESSION['debugRole']);
-        } else {
-            $userType = new UserType("user");
+if ($showDebug) {
+    // Debug login
+    if (isset($_GET['debugLogin'])) {
+        $_SESSION['debugLogin'] = $_GET['debugLogin'];
+    }
+    if (isset($_SESSION['debugLogin'])) {
+        $loggedIn = $_SESSION['debugLogin'];
+        // Debug role (Only available if user is logged in)
+        if ($loggedIn) {
+            if (isset($_GET['debugRole'])) {
+                $_SESSION['debugRole'] = $_GET['debugRole'];
+            }
+            if (isset($_SESSION['debugRole'])) {
+                $userType = new UserType($_SESSION['debugRole']);
+            } else {
+                $userType = new UserType("user");
+            }
         }
     }
+    // Print debugging status
+    echo "DEBUGGING ENABLED<br>LOGGED IN: <b>" . ($loggedIn ? "YES" : "NO") . "</b><br>ROLE: <b>" . (($userType != null) ? $userType->getTypeString() : "none") . "</b>";
 }
-// Print debugging status
-echo "DEBUGGING ENABLED<br>LOGGED IN: <b>" . ($loggedIn ? "YES" : "NO") . "</b><br>ROLE: <b>" . (($userType != null) ? $userType->getTypeString() : "none") . "</b>";
-
 
 //print current user 
 if (isset($user)) {
@@ -81,7 +84,7 @@ if (isset($user)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ITP-Minigames</title>
 
-    <!-- IMPORT BOOTSTRAP -->    
+    <!-- IMPORT BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
@@ -95,9 +98,9 @@ if (isset($user)) {
     <link rel="stylesheet" type="text/css" href="css/forum.css">
 
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-    
-    <link rel="stylesheet" type="text/css" href="css/userAdministration.css"/>
-    <link rel="stylesheet" type="text/css" href="css/game.css"/>
+
+    <link rel="stylesheet" type="text/css" href="css/userAdministration.css" />
+    <link rel="stylesheet" type="text/css" href="css/game.css" />
 
 
     <!-- IMPORT JS -->
@@ -105,26 +108,70 @@ if (isset($user)) {
 </head>
 
 <body>
-    <!-- Testing ground -->
-    <div class="ps-3 mt-3 pt-2 border-top">
-        <p class="h5">Components</p>
-        <a href="index.php?action=showUsers&amount=20&offset=0" class="btn btn-success">User List</a>
-        <a href="index.php?action=viewGame&id=1" class="btn btn-success">View Game</a>
+
+    <nav class="navbar navbar-dark bg-dark">
+
         <a href="index.php?action=viewFrontPage" class="btn btn-success">View Front Page</a>
         <a href="index.php?action=forum" class="btn btn-success">Forum</a>
-        <a href="index.php?action=listCreatedGames" class="btn btn-success">Created Games List</a>
-        <a href="index.php?action=register" class="btn btn-success">Registration</a>
-        <a href="index.php?action=login" class="btn btn-success">Login</a>
-        <a href="index.php?action=logout" class="btn btn-success">Logout</a>
 
-    </div>
-    <div class="ps-3 mt-2 mb-3 pb-3 border-bottom">
-        <p class="h5">Roles</p>
-        <a href="index.php?debugLogin=<?=!$loggedIn?>" class="btn btn-success">Toggle Login</a>
-        <a href="index.php?debugRole=user" class="btn btn-success">User Role</a>
-        <a href="index.php?debugRole=creator" class="btn btn-success">Creator Role</a>
-        <a href="index.php?debugRole=admin" class="btn btn-success">Admin Role</a>
-    </div>
+        <?php
+        if ($loggedIn) {
+            $accessStrength = $userType->getAccessStrength();
+            // Normal user components
+            if ($accessStrength >= UserType::User()->getAccessStrength()) {
+        ?>
+                <a href="index.php?action=logout" class="btn btn-success">Logout</a>
+            <?php
+            }
+            // Game creator components
+            if ($accessStrength >= UserType::Creator()->getAccessStrength()) {
+            ?>
+                <a href="index.php?action=listCreatedGames" class="btn btn-success">Created Games List</a>
+            <?php
+
+            }
+            // Admin components
+            if ($accessStrength >= UserType::Admin()->getAccessStrength()) {
+            ?> <a href="index.php?action=showUsers&amount=20&offset=0" class="btn btn-success">User List</a>
+
+            <?php
+            }
+        } else {
+            // if someone isn´t logged in
+            ?>
+            <a href="index.php?action=register" class="btn btn-success">Registration</a>
+            <a href="index.php?action=login" class="btn btn-success">Login</a>
+        <?php
+        }
+        ?>
+    </nav>
+
+    <?php
+    if ($showDebug) {
+    ?> <div class="ps-3 mt-3 pt-2 border-top">
+
+            <p class="h5">Components</p>
+
+            <a href="index.php?action=showUsers&amount=20&offset=0" class="btn btn-success">User List</a>
+            <a href="index.php?action=viewGame&id=1" class="btn btn-success">View Game</a>
+            <a href="index.php?action=viewFrontPage" class="btn btn-success">View Front Page</a>
+            <a href="index.php?action=forum" class="btn btn-success">Forum</a>
+            <a href="index.php?action=listCreatedGames" class="btn btn-success">Created Games List</a>
+            <a href="index.php?action=register" class="btn btn-success">Registration</a>
+            <a href="index.php?action=login" class="btn btn-success">Login</a>
+            <a href="index.php?action=logout" class="btn btn-success">Logout</a>
+
+        </div>
+        <div class="ps-3 mt-2 mb-3 pb-3 border-bottom">
+            <p class="h5">Roles</p>
+            <a href="index.php?debugLogin=<?= !$loggedIn ?>" class="btn btn-success">Toggle Login</a>
+            <a href="index.php?debugRole=user" class="btn btn-success">User Role</a>
+            <a href="index.php?debugRole=creator" class="btn btn-success">Creator Role</a>
+            <a href="index.php?debugRole=admin" class="btn btn-success">Admin Role</a>
+        </div>
+    <?php
+    } ?>
+
     <!-- Main container -->
     <div class="container">
         <?php
@@ -145,7 +192,7 @@ if (isset($user)) {
         if ($loggedIn) {
             $accessStrength = $userType->getAccessStrength();
             // Normal user components
-            if ($accessStrength >= UserType::User()->getAccessStrength()) {                
+            if ($accessStrength >= UserType::User()->getAccessStrength()) {
             }
             // Game creator components
             if ($accessStrength >= UserType::Creator()->getAccessStrength()) {
