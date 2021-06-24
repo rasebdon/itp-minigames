5bb2cebcacfbb845c9e4a875ec8b76044255ccc3
Tobi <to.meindl@gmx.at>
2021-06-24 17:34:32 +0200

    fixed refresh bug for favorite button

--- utility/favoritelogic.php
+++ utility/favoritelogic.php
@@ -2,11 +2,13 @@
 if(isset($_POST['addFavorite'])){
     //var_dump($_POST['addFavorite']);
     FavoriteService::$instance->insertFavorite($_POST['addFavorite'], $_SESSION['UserID']);
+    header('Location: index.php?action=viewGame&id=' . $_GET['id']);
 }
 
 if(isset($_POST['removeFavorite'])){
     //var_dump($_POST['removeFavorite']);
     FavoriteService::$instance->removeFavorite($_POST['removeFavorite'], $_SESSION['UserID']);
+    header('Location: index.php?action=viewGame&id=' . $_GET['id']);
 }
 
 
