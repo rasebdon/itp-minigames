4c4cecafdd5b014c959f7173c6f90fdd6e9dced5
Michael Duskanich <michael.duskanich@gmail.com>
2021-06-27 22:30:21 +0200

    Design - Polished forum main page
    also fixed animation in game list

--- components/ForumMainPage.component.php
+++ components/ForumMainPage.component.php
@@ -13,36 +13,23 @@ class ForumMainPageComponent
 
 
 
-                if (isset($_SESSION['UserID']) &&  $_SESSION['UserID'] != null) {
-                    if (UserService::$instance->getUser($_SESSION['UserID'])->getUserType()->getAccessStrength() >= UserType::Creator()->getAccessStrength()) {
-?>
-
-                        <div onclick="location.href='index.php?action=forum&id=1';" class="heading-primary">
-
-                            <h1 class="heading-primary__text">Developer Forum</h1>
-
-
-                            <!--<h1 class="m-0">
-                                <span class="d-inline-block">Developer Forum</span>
-                                <span class="d-inline-block text-muted forum-banner-author"> </span>
-                            </h1>
-                            <h4 class="m-0"><span class="d-inline-block d-none d-md-block">
-                                    Posts:
-                                    <?= ForumService::$instance->getNumberOfPosts(1) ?>
-                                </span></h2>-->
-                        </div>
-                <?php
-                    }
-                }
-
-
                 //display a banner for each game with link to game forum 
-                ?>
+?>
                 <div class="heading-primary">
                     <h1 class="heading-primary__text">Forum</h1>
                 </div>
-
                 <div class="forum-list">
+                    <?php
+                    if (isset($_SESSION['UserID']) &&  $_SESSION['UserID'] != null) {
+                        if (UserService::$instance->getUser($_SESSION['UserID'])->getUserType()->getAccessStrength() >= UserType::Creator()->getAccessStrength()) {
+                    ?>
+                            <div class="forum forum-banner md-12 mb-3  justify-content-between" onclick="location.href='index.php?action=forum&id=1';">
+                                <h2 class="heading-secondary center">Developer Forum</h2>
+                            </div>
+                    <?php
+                        }
+                    }
+                    ?>
                     <?php
 
                     if ($games == null) {
@@ -53,30 +40,30 @@ class ForumMainPageComponent
                     }
                     foreach ($games as $game) {
                     ?>
+                        <div class="forum forum-banner md-12 mb-3 justify-content-between" onclick="location.href='index.php?action=forum&id=<?= GameService::$instance->getForumID($game) ?>';">
 
+                            <div class="row">
 
-                        <div class="forum forum-banner md-12 mb-3 row justify-content-between" onclick="location.href='index.php?action=forum&id=<?= GameService::$instance->getForumID($game) ?>';">
-                            <h1 class="m-0 col-11 " style="line-break: anywhere;">
-                                <span class="d-inline-block"><?= $game->getTitle() ?></span>
-                                <span class="d-inline-block text-muted forum-banner-author">by
-                                    <?= $game->getAuthor()->getUsername(); ?>
-                                </span>
-                            </h1>
+                                <div class="col-12 col-lg-8">
+                                    <h2 class="heading-secondary d-inline-block"><?= $game->getTitle() ?></h2>
+                                    <span class="d-inline-block text-muted forum-banner-author">by
+                                        <?= $game->getAuthor()->getUsername(); ?>
+                                    </span>
+                                </div>
 
-                            <h4 class="m-0 col-1"><span class="d-inline-block d-none d-md-block">
-                                    Posts:
-                                    <?= ForumService::$instance->getNumberOfPosts(GameService::$instance->getForumID($game)) ?>
-                                </span></h4>
+                                <div class="col-12 col-lg-4 right">
+                                    <span class="d-inline-block">
+                                        Posts:
+                                        <?= ForumService::$instance->getNumberOfPosts(GameService::$instance->getForumID($game)) ?>
+                                    </span>
+                                </div>
+                            </div>
 
-                            <div class="img-wrap col-4" style="width:100%; height: 200px; background: url(<?= $game->getFirstScreenshot() ?>);
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;"></div>
+                            <div>
+                                <img class="forum-banner-image" src="<?= $game->getFirstScreenshot() ?>" alt="game screenshot">
+                            </div>
                         </div>
-
     <?php
-
-
                     }
                     echo "</div>";
                 } else {
